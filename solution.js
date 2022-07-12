eb58a958cb1be32d40643d3d7ece00ff68c8564c
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-07-12 15:28:52 +0200

    exercises - program repetition

---
+++ solution.js
@@ -0,0 +1,22 @@
+let i = 0;
+while (i < 15) {
+    console.log(i);
+    i++
+    }
+
+let j = 1;
+let sum = 0;
+    while (j <= 20) {
+        sum = sum + j;
+        console.log(sum);
+        j++;
+    }
+
+
+    let k = 0;
+    
+    do {
+        console.log('The number is', k);
+ k++
+       
+     } while(k < 20);
